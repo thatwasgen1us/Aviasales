39dc991d88dc34b114153723eb27a29a0da96dfe
Егор Половко <skelletrise@gmail.com>
2025-02-05 02:57:38 +0700

    third commit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { countReducer } from './reducers/countReducer';
 import filterReducer from './reducers/filterReducer';
-import { ticketReducer } from './reducers/ticketReducer';
 import sortingReducer from './reducers/sortingSlice';
+import { ticketReducer } from './reducers/ticketReducer';
 
+const rootReducer = combineReducers({
+  filters: filterReducer,
+  counter: countReducer,
+  tickets: ticketReducer,
+  sorting: sortingReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    filters: filterReducer,
-    counter: countReducer,
-    tickets: ticketReducer,
-    sorting: sortingReducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
